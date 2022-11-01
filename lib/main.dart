b4bf1ac03d5b67e4171a364345b227de7a26daee
zakibn <zakariachafai77@gmail.com>
2022-11-01 22:49:58 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// Sliding Puzzle - Flutter - Speed Code
+
+// Pola's Code
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Board.dart';
+
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  await SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(SlidingPuzzle());
+}
+
+class SlidingPuzzle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Sliding Puzzle",
+      debugShowCheckedModeBanner: false,
+      home: Board(),
+    );
+  }
+}
